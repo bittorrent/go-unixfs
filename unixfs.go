f299fab750393f826e2f9dba5a69e625c4d635c8
Eric Chen <eric@tron.network>
2020-05-13 20:31:38 -0700

    Fix test size failure for token meta

--- unixfs.go
+++ unixfs.go
@@ -164,7 +164,7 @@ func size(pbdata *pb.Data) (uint64, error) {
 	switch pbdata.GetType() {
 	case pb.Data_Directory, pb.Data_HAMTShard:
 		return 0, errors.New("can't get data size of directory")
-	case pb.Data_File:
+	case pb.Data_File, pb.Data_TokenMeta:
 		return pbdata.GetFilesize(), nil
 	case pb.Data_Symlink, pb.Data_Raw:
 		return uint64(len(pbdata.GetData())), nil
